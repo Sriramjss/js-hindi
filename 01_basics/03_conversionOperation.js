3d7f65f45d522b907a2b1be8af5d5d8474398369
Sriramjss <94704419+Sriramjss@users.noreply.github.com>
2023-10-27 13:51:25 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = 33
+
+console.log(typeof score)
+
+let valueInNum = Number(score)
+console.log(typeof valueInNum)
+console.log( valueInNum)
+
+
+// 33-> 33
+// 33ac-> nan
+// true = 1
+
+let islog = 4
+let isnt  = Boolean(islog)
+console.log(isnt)
+console.log(typeof isnt)           // 1 is true  and 0 is false empty is false and string is true
+
+
+let a= 33
+let b = String(a)
+console.log(b)
+console.log(typeof b)
