77da28e91b1dfa5c8f6143f2f5b5633b591f04b2
Sriramjss <94704419+Sriramjss@users.noreply.github.com>
2023-10-27 15:11:30 +0000

    arr obj fun

---
+++ 01_basics/referenceDatatypes.js
@@ -0,0 +1,31 @@
+
+
+
+const score = 100
+const scorevalue = 100.5
+const loggin = true 
+const temp = null
+let user = undefined
+
+const id = Symbol('22')
+const idd = Symbol('22')
+
+console.log(id === idd);
+
+const nigNum = 3737473842848432342n
+
+
+// array , objects , functions       // these all are call by reference   // its dattype is called functoin
+
+const heros = ["shaktima" , " dogd" , "mannn"]
+let myObj= {
+    name: "raam",
+    age: 78
+}
+
+
+const myFunction = function() {
+    console.log("hello world");
+}
+
+console.log(typeof heros);
