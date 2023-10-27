147001d904aae6453c79ff7811b3dbed3f841f00
Sriramjss <94704419+Sriramjss@users.noreply.github.com>
2023-10-27 12:03:17 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 1515454
+let accountEmail = "sriram.com"   // use let fro variables and dont use var 
+var accountPassword = "78787"
+accountCity = "banglore"
+let accountState                  // variables must have a value it cannot be blank
+
+// accountId = 2
+accountEmail = "hihih.com"
+accountPassword = "454545"
+accountCity = 'mang'
+
+
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword , accountCity, accountState]);
+
+
+
