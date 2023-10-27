8d8d2b1ee11c36face2b2f2d5f260ba3045600ff
Sriramjss <94704419+Sriramjss@users.noreply.github.com>
2023-10-27 14:47:39 +0000

    comparing the opertors

---
+++ 01_basics/04_compaarision.js
@@ -0,0 +1,19 @@
+console.log(1 < 2)
+console.log(1 >= 2)
+console.log(1 != 2)
+
+console.log("01" < 2)
+
+console.log(1 == 2)
+
+
+console.log(null >= 0);          // this takes null as 0
+console.log(null < 0);
+console.log(null == 0);
+
+console.log(undefined == 0);
+console.log(undefined < 0);       // false for every case
+console.log(undefined >= 0);
+
+// ===    this is called strictly equal
+console.log("2" === 2);    // this checks the data type as well
