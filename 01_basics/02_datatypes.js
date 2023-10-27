cb6c7b0be0a2948cb8579f654e9f7bfc7bf2b727
Sriramjss <94704419+Sriramjss@users.noreply.github.com>
2023-10-27 12:09:02 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";   // use all codes as new vesions
+
+// alert("hello")                 we are useing nodejs not browzer
+// alert( 3 +3 )
+
+console.log(3 + 3);
+console.log('ram')
+
+
+let name = "ranndo"
+let age = 45
+let logged = false
+let temp = null
+
+ // console.log(temp)
+
+// bigint
+// Number
+// string               these are the data types
+// Boolean
+// null
+// undifined
+// symbol is used fro unique
+// object 
+console.log(typeof name,typeof temp);      // null is object when used typeof
