0ef84073ed330dfc1870b762999a3cf40ee51792
Sriramjss <94704419+Sriramjss@users.noreply.github.com>
2023-10-27 14:33:37 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,3 +21,28 @@ let a= 33
 let b = String(a)
 console.log(b)
 console.log(typeof b)
+
+
+//// operration ////
+
+
+let xa = 56
+let xb = -xa
+console.log(xb)
+
+//  STRING ADDITION
+let ab = "hello"
+let cd = " ram"
+let ds = ab + cd
+console.log(ds)
+
+
+// COMPLEX TYPES FOR STRING AND NUMBERS 
+console.log("1" + 2)
+console.log("1" + "2")
+console.log(1 + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 3)    
+console.log(1 + 2 + "3")      // prefer not to code this way its not very user friendly
+
+console.log(true)
