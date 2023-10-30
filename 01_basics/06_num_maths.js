3cfbbd2ae539996c12eaf6d732da743e1f98e869
Sriramjss <94704419+Sriramjss@users.noreply.github.com>
2023-10-30 11:18:28 +0000

    math and  num

---
+++ 01_basics/06_num_maths.js
@@ -0,0 +1,32 @@
+const score = 445
+//console.log(score);
+const balance = new Number(100.34343)
+// console.log(balance);
+
+// console.log(balance.toString().length);     // defines the length and string converts into string 
+// console.log(balance.toFixed(2));            //fixes after the decimal number
+
+const myOne = 24545443.34878
+// console.log(myOne.toPrecision(3));
+
+// console.log(myOne.toLocaleString('en-IN'));
+
+///// MATHS  /////
+let x = 4.3
+console.log(Math);
+console.log(Math.abs(-234))
+console.log(Math.round(x));
+console.log(Math.ceil(x));
+console.log(Math.floor(x));
+console.log(Math.min(4,8,7,9,1));
+
+
+console.log((Math.random()*10)+1);
+console.log(Math.round(Math.random()*10)+1);
+
+
+let min = 10
+let max = 20
+
+console.log(Math.floor(Math.random() * (max - min +1) + min))      // imp an be used as a formula
+
