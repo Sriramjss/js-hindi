b8ad1ae1a56713d404aac621a45cb950c2ebb370
Sriramjss <94704419+Sriramjss@users.noreply.github.com>
2023-11-01 10:16:03 +0000

    01 array

---
+++ 02_basics/01_array.js
@@ -0,0 +1,36 @@
+const myArray = [0 , 1, 2, 3, 4, 5]
+const  myHero  = ["raamn"  , "bhat"]
+const arr2 = new Array(1,34,5,56)
+// console.log(myArray[1])
+// console.log(arr2[1])
+
+// array methods 
+myArray.push(7);
+myArray.push(34);
+//console.log(myArray);
+myArray.pop()
+//console.log(myArray);
+
+//myArray.unshift(66)
+myArray.shift()
+// console.log(myArray);
+
+// console.log(myArray.includes(3));
+// console.log(myArray.indexOf(3));
+
+
+ // let arrra2 = myArray.join()
+ 
+// console.log(arrra2);
+// console.log(typeof arrra2);
+
+// slice , splice
+let mine = myArray.slice(1,4)
+console.log("A ", myArray);
+console.log(mine);
+console.log("b ", myArray);
+let minee = myArray.splice(1,4)
+console.log(minee);
+console.log("C ", myArray);
+// console.log(myArray);
+
