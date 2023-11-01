0028aab7182cc6120783a04a2a89e88d7d9e73b8
Sriramjss <94704419+Sriramjss@users.noreply.github.com>
2023-11-01 09:26:07 +0000

    date and time

---
+++ 01_basics/07_date_time.js
@@ -0,0 +1,19 @@
+// dates
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+let mycredate = new Date(2023,0,14,5,6)          // moyths start from 0 fin js
+// console.log(mycredate.toLocaleString());
+
+
+let mytineStamp =  Date.now()
+// console.log(mycredate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+
